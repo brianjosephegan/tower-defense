fecda8ca03bd4c1a6a87c64db97758f057309607
Brian Egan <brianjoseph.egan@gmail.com>
2021-07-27 22:17:37 -0700

    Basic Grid and Enemy
    Add basic grid system and an enemy with a preconfigured path to take.

---
+++ Assets/Enemy/EnemyMover.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintWayPointName();
+    }
+
+    private void PrintWayPointName()
+    {
+        foreach (var waypoint in path)
+        {
+            Debug.Log(waypoint.name);
+        }
+    }
+}
