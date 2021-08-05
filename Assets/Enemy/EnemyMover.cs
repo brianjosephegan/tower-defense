48367a5d2c7d6744abf899e6156bde87759d7871
Brian Egan <brianjoseph.egan@gmail.com>
2021-08-05 11:19:03 -0700

    Create Enemy and LERP Movement
    Create new Enemy Prefab and LERP movement between waypoints so the movement is smooth.

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
 
     void Start()
     {
@@ -16,8 +16,17 @@ public class EnemyMover : MonoBehaviour
     {
         foreach (Waypoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                Vector3 lerpedPosition = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                transform.position = new Vector3(lerpedPosition.x, transform.position.y, lerpedPosition.z);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
